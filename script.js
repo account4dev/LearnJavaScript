010975f81ef00538895ce545b7a3ecca5cbe53a0
account4dev <code.development@ya.ru>
2025-09-19 20:11:57 +0300

    Урок 3

---
+++ script.js
@@ -0,0 +1,7 @@
+let lastName = prompt("Введите Вашу фамилию:"); 
+let firstName = prompt("Введите Ваше имя:"); 
+let patronymic = prompt("Введите Ваше отчество:"); 
+
+console.log(lastName); 
+console.log(firstName); 
+console.log(patronymic);
