7efb559411e3933602dca1c3c11e13bd335365cb
account4dev <code.development@ya.ru>
2025-10-19 12:07:02 +0300

    Урок 8

--- script.js
+++ script.js
@@ -1,46 +1,82 @@
-// 1. Базовые преобразования
-const message1 = "JavaScript is AWESOME";
-console.log(message1.toUpperCase());
+// 1. Проверка возраста
+const age = prompt("Введите свой возраст:");
+if (age < 18) {
+  alert("Доступ запрещён");
+} else {
+  alert("Доступ разрешён");
+}
 
 
-// 2. Извлечение подстрок
-const date = "2024-07-30";
-const dateArray = date.split("-");
-
-const year = "Год: " + dateArray[0];
-const month = "Месяц: " + dateArray[1];
-const day = "День: " + dateArray[2];
-
-console.log(year, "/", month, "/", day);
+// 2. Чётное или нечётное
+const number = prompt("Введите число:");
+console.log("Число чётное:", (number % 2 == 0) ? true : false);
 
 
-// 3. Поиск и замена
-let message2 = "Я люблю JavaScript! Я люблю это дело.";
-message2 = message2.replace("JavaScript", "программирование");
-message2 = message2.replaceAll("люблю", "обожаю");
+// 3. Калькулятор
+const firstNumber = Number(prompt("Введите первое число:"));
+const secondNumber = Number(prompt("Введите второе число:"));
+let operator = prompt("Введите оператор (+, -, * или /):");
 
-console.log(message2);
+let result;
+
+switch(operator) {
+  case "+":
+    result = firstNumber + secondNumber;
+    break;
+  case "-":
+    result = firstNumber - secondNumber;
+    break;
+  case "*":
+    result = firstNumber * secondNumber;
+    break;
+  case "/":
+    result = firstNumber / secondNumber;
+    break;
+  default:
+    operator = "+";
+    result = firstNumber + secondNumber;
+}
+
+alert(`${firstNumber} ${operator} ${secondNumber} = ${result}`);
 
 
-// 4. Работа с разделителями
-const food = "яблоки,груши,виноград";
-console.log(food.replaceAll(",", " | ").toUpperCase());
+// 4. Проверка длины строки
+const string = prompt("Введите строку:");
+
+if (string.length > 10) {
+  let encryptedPart = "*".repeat(string.length - 5);
+  alert(string.slice(0, 5) + encryptedPart);
+} else {
+  alert(string.toUpperCase());
+}
 
 
-// 5. Проверка содержимого
-const email = "example@mail.com";
-console.log("Есть ли в строке символ \"@\"?", email.includes("@"));
-console.log("Оканчивается ли строка на \".com\"?", email.endsWith(".com"));
+// 5. Определение времени суток
+const hour = prompt("Который сейчас час?");
+if (hour >= 0 && hour <= 5) {
+  alert("Ночь");
+} else if (hour >= 6 && hour <= 11) {
+  alert("Утро");
+} else if (hour >= 12 && hour <= 17) {
+  alert("День");
+} else {
+  alert("Вечер");
+}
 
 
-// 6. Маскирование данных
-const cardNumber = "1234567812345678";
-console.log(cardNumber.slice(0, 4), "**** ****", cardNumber.slice(-4));
+// 6. Проверка e-mail
+const email = prompt("Введите email-адрес:");
+if (email.includes("@") && email.includes(".")) {
+  alert("Верный формат");
+} else {
+  alert("Ошибка!");
+}
 
 
-// 7. Анализ строки
-const message3 = "Hello World!";
-console.log("Длина строки:", message3.length);
-console.log("Первый символ:", message3.slice(0, 1));
-console.log("Последний символ:", message3.slice(-1));
-console.log("Символ посередине:", message3.slice(message3.length / 2, message3.length / 2 + 1));
+// 7. Палиндром (базовый)
+const palindrome = prompt("Введите слово:");
+if (palindrome == palindrome.split("").reverse().join("")) {
+  alert("Это палиндром");
+} else {
+  alert("Это не палиндром");
+}
